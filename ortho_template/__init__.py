af1262ee269a2b84a4af19a4557e90834ff8a129
cabinetdrdicembre-web <cabinet.dr.dicembre@gmail.com>
2026-07-02 12:25:49 +0200

    Create __init__.py

---
+++ ortho_template/__init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "Ortho Template",
+    "author": "Cabinet Dr Dicembre + ChatGPT",
+    "version": (0, 1, 0),
+    "blender": (4, 0, 0),
+    "location": "View3D > Sidebar",
+    "description": "Orthodontic Template Generator",
+    "category": "3D View",
+}
