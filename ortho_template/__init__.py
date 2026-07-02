adf19a1a9b5f0f07750523d8b6ed39c133f642e6
cabinetdrdicembre-web <cabinet.dr.dicembre@gmail.com>
2026-07-02 12:28:17 +0200

    __init__.py

--- ortho_template/__init__.py
+++ ortho_template/__init__.py
@@ -4,6 +4,24 @@ bl_info = {
     "version": (0, 1, 0),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar",
-    "description": "Orthodontic Template Generator",
+    "description": "Orthodontic 5 Views Template",
     "category": "3D View",
 }
+
+import bpy
+
+from . import panel
+
+
+def register():
+
+    panel.register()
+
+
+def unregister():
+
+    panel.unregister()
+
+
+if __name__ == "__main__":
+    register()
