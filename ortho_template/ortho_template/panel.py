a54b561575e857f58ee98dcfa11590407e69afaa
cabinetdrdicembre-web <cabinet.dr.dicembre@gmail.com>
2026-07-02 12:30:15 +0200

    Update panel.py

--- ortho_template/ortho_template/panel.py
+++ ortho_template/ortho_template/panel.py
@@ -2,33 +2,36 @@ import bpy
 
 
 class ORTHO_PT_MainPanel(bpy.types.Panel):
+    """Panneau principal de l'add-on"""
+
     bl_label = "Ortho Template"
-    bl_idname = "ORTHO_PT_mainpanel"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Ortho'
+    bl_idname = "ORTHO_PT_main_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Ortho"
 
     def draw(self, context):
 
         layout = self.layout
 
-        layout.label(text="Orthodontic Template")
+        layout.label(text="Bienvenue !")
+        layout.label(text="Le projet est en cours de développement.")
 
         layout.separator()
 
-        layout.operator(
-            "ortho.import_stl",
-            icon='IMPORT'
-        )
+        layout.label(text="Version : 0.1")
 
-        layout.separator()
 
-        layout.operator(
-            "ortho.create_cameras",
-            icon='CAMERA_DATA'
-        )
+classes = (
+    ORTHO_PT_MainPanel,
+)
 
-        layout.operator(
-            "ortho.render_views",
-            icon='RENDER_STILL'
-        )
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
